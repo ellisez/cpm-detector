f47dcf81cd2a1a06a8094637f9a66f57f6bccdba
ellise <ellise@qq.com>
2019-12-13 14:34:59 +0800

    init

---
+++ index.js
@@ -0,0 +1,83 @@
+const path=require('path');
+const fs=require('fs');
+
+class CpmDetector {
+    constructor(options) {
+        this.options=options;
+        this.cacheRuntime=path.resolve(process.cwd(), './.cpmrc');
+        this.load();
+    }
+
+    load() {
+        this.data={};
+    }
+
+    write(update) {
+        function mergeContainer(container, updateContainer) {
+            for (let i=0; i<updateContainer.length; i++) {
+                const updateComponent=updateContainer[i];
+                for (let j=0; j<container.length; j++) {
+                    const component=container[j];
+                    if (updateComponent.name==component.name) {
+                        mergeComponent(component, updateComponent);
+                        break;
+                    }
+                }
+            }
+        }
+        function mergeComponent(component, updateComponent) {
+            for (let file in updateComponent.sources) {
+                const sources=component.sources[file];
+                const updateSources=updateComponent.sources[file];
+                if (updateSources) {
+                    component.sources[file]=updateSources;
+                }
+            }
+        }
+
+        for (let containerKey in update) {
+            let container=this.data[containerKey];
+            let updateContainer=update[containerKey];
+            if (containerKey==='parent') {
+                mergeComponent(container, updateContainer);
+            } else {
+                if (container && updateContainer) {
+                    mergeContainer(container, updateContainer);
+                } else if (updateContainer){
+                    this.data[containerKey]=updateContainer;
+                }
+            }
+
+        }
+    }
+
+    save() {
+        try {
+            let code=JSON.stringify(this.data, null, '\t');
+            fs.writeFileSync(this.cacheRuntime, code);
+        } catch (e) {
+
+        }
+    }
+
+    remove() {
+        try {
+            fs.unlinkSync(this.cacheRuntime);
+        } catch (e) {
+
+        }
+    }
+
+    apply(compiler) {
+        const self=this;
+
+        compiler.plugin('cpm-component', function (data) {
+            self.write(data);
+        });
+        compiler.plugin('done',function(compilation) {
+            self.save();
+        });
+    }
+}
+module.exports=CpmDetector;
+module.exports.default=CpmDetector;
